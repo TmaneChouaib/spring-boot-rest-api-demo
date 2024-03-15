acdf4b48997aa71a829ec2d7883ca114b9c76477
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-03-15 15:09:05 +0000

    Added personnage entity

---
+++ src/main/java/com/tmane/restappdemo/entiy/personnage.java
@@ -0,0 +1,18 @@
+package com.tmane.restappdemo.entiy;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class personnage {
+    private String name;
+    private String occupation;
+    private String religion;
+    private LocalDate birthDate;
+    private LocalDate deceaseDate;
+    private String placeOfBirth;
+    private String nationality;
+    private String contribution;
+    private String quotes;
+    private String biographie;
+}
