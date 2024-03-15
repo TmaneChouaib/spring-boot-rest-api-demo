a8048a9650c254ab2952bb4f63adf6b85fb93924
TmaneChouaib <chouaib.tmane@hotmail.com>
2024-03-15 14:41:38 +0000

    first commit

---
+++ src/main/java/com/tmane/restappdemo/RestAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tmane.restappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmane/restappdemo/RestAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmane.restappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
